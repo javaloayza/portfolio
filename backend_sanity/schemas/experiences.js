df70946de72db5d8140b101f87e021f650597ea8
Javadeveloper7 <95065154+Javadeveloper7@users.noreply.github.com>
2023-07-10 23:51:41 -0500

    date fix

--- backend_sanity/schemas/experiences.js
+++ backend_sanity/schemas/experiences.js
@@ -1,4 +1,65 @@
-export default{
+export default {
+  name: "experiences",
+  title: "Experiences",
+  type: "document",
+  fields: [
+    /* {
+        name: 'year',
+        title: 'Year',
+        type: 'date',
+        options: {
+          dateFormat: 'YYYY-MM-DD',
+          calendarTodayLabel: 'Today'
+        }
+      }, */
+    {
+      name: "years",
+      title: "Years",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              name: "startDate",
+              title: "Start Date",
+              type: "date",
+              options: {
+                dateFormat: "YYYY-MM-DD",
+                calendarTodayLabel: "Today",
+              },
+            },
+            {
+              name: "endDate",
+              title: "End Date",
+              type: "date",
+              options: {
+                dateFormat: "YYYY-MM-DD",
+                calendarTodayLabel: "Today",
+              },
+            },
+            // Otros campos relacionados con la experiencia laboral
+          ],
+        },
+      ],
+    },
+    {
+      name: "works",
+      title: "Works",
+      type: "array",
+      of: [{ type: "workExperience" }],
+    },
+  ],
+  orderings: [
+    {
+      title: "Most Recent",
+      name: "mostRecent",
+      by: [{ field: "years.startDate", direction: "desc" }],
+    },
+  ],
+};
+
+/* export default{
     name:'experiences',
     title:'Experiences',
     type: 'document',
@@ -15,4 +76,4 @@ export default{
             of:[{ type:'workExperience'}]
         },
     ]
-}
+} */
