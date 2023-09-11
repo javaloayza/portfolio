ff2e7dde11708705f713d9fc89ca5852396248ad
javaloayza <java.loayza@gmail.com>
2023-09-10 23:40:15 -0500

    animations-1

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
       >
         <div className='app__header-badge'>
           <div className='badge-cmp app__flex'>
-            <span>👋</span>
+            <span className="waveEmoji">👋</span>
             <div style={{ marginLeft: 20}}>
               <p className='p-text'>Hello, I am</p>
               <h1 className='head-text'>Aldo</h1>
