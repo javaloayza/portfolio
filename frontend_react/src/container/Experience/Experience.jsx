c06c9b121ad1e6335cf3a777a9ffc1abb9718282
javaloayza <java.loayza@gmail.com>
2024-09-11 00:49:18 -0500

    exp-order

--- frontend_react/src/container/Experience/Experience.jsx
+++ frontend_react/src/container/Experience/Experience.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
 import Modal from 'react-modal';
 import { AppWrap, MotionWrap } from '../../wrapper';
+import { ReactComponent as Open } from "../../assets/open.svg";
 import { client } from '../../client';
 
 import './Experience.scss';
@@ -31,14 +32,21 @@ const Experience = () => {
     return dateB - dateA;
   });
 
-  const formatDate = (date) => {
-    if (date) {
-      return new Date(date).toLocaleDateString('es-ES', {
-        month: '2-digit',
-        year: 'numeric'
-      }).replace(/\//g, '/');
+  const formatDate = (dateString) => {
+    if (dateString) { // Verifica si dateString existe (no es null, undefined, o vacío)
+      const dateObject = new Date(dateString); // Crea un objeto Date a partir del string
+  
+      if (isNaN(dateObject)) { // Comprueba si la fecha creada es inválida (NaN)
+        console.error("Fecha inválida:", dateString); // Imprime un error si la fecha no es válida
+        return ''; // Devuelve una cadena vacía si la fecha no es válida
+      }
+  
+      const month = (dateObject.getMonth() + 1).toString().padStart(2, '0'); // Obtiene el mes (0-11), le suma 1 y formatea a dos dígitos
+      const year = dateObject.getFullYear(); // Obtiene el año
+  
+      return `${month}/${year}`; // Devuelve la fecha formateada como MM/AAAA
     }
-    return '';
+    return ''; // Devuelve una cadena vacía si dateString no existe
   };
 
   const handleWorkClick = (work) => {
@@ -93,6 +101,7 @@ const Experience = () => {
                   data-tip
                   data-for={`experience-${index}`}
                 > */}
+
             <div className="app__skills-exp-year">
               <p className="bold-text">
                 {experience.years?.length > 0 &&
@@ -103,17 +112,21 @@ const Experience = () => {
                   }`}
               </p>
             </div>
-            <motion.div className="app__skills-exp-works">
+            <motion.div className="app__skills-exp-experiences">
               {experience.works.map((work) => (
                 <motion.div
                   whileInView={{ opacity: [0, 1] }}
                   transition={{ duration: 0.5 }}
-                  className="app__skills-exp-work"
+                  className="app__skills-experiences"
                   key={work.name}
                 >
                   <h4 className="bold-text">{work.name}</h4>
                   <p className="p-text">{work.company}</p>
-                </motion.div>
+                  <button type="button" className="p-text button_details" onClick={() => handleWorkClick(experience.works[0])} >
+                  <div className='open_style'><Open /></div>  
+                    View more
+                  </button>
+              </motion.div>
               ))}
             </motion.div>
             <ReactTooltip
